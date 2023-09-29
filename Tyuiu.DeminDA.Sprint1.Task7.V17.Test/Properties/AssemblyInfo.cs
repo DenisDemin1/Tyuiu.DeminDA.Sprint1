153e59a2183efd0436e080b0cb650c934a3ca8a7
DeminDenis1 <maxwelldenis888@gmail.com>
2023-09-29 22:49:03 +0500

    Task7

---
+++ Tyuiu.DeminDA.Sprint1.Task7.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DeminDA.Sprint1.Task7.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DeminDA.Sprint1.Task7.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("32d48c75-49d0-42c0-a5ff-487ac27ca5bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.DeminDA.Sprint1.Task7.V17/Program.cs
+++ Tyuiu.DeminDA.Sprint1.Task7.V17/Program.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.DeminDA.Sprint1.Task0.V2.Lib;
+using Tyuiu.DeminDA.Sprint1.Task7.V17.Lib;
 
-//Задание
-//Написать программу, которая вычисляет выражение 3/3*2+12/4/3-2 и печатает результат на экране.
-
-namespace Tyuiu.DeminDA.Sprint1.Task0.V2
+namespace Tyuiu.DeminDA.Sprint1.Task7.V17
 {
     class Program
     {
@@ -17,29 +14,38 @@ namespace Tyuiu.DeminDA.Sprint1.Task0.V2
         {
             DataService ds = new DataService();
 
-            Console.Title = "Спринт 1 | Выполнил: Дёмин Д.А. | ИИПБ-23-2";
+            Console.Title = "Спринт #0 | Выполнил: Дёмин Д.А. | ИИПБ-23-2";
             //Длина строки 75 символов
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
             Console.WriteLine("* Выполнил: Дёмин Денис Александрович | ИИПБ-23-2                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая вычисляет выражение 3/3*2+12/4/3-2          *");
-            Console.WriteLine("* и печататает результат на экране                                        *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных ,вводимых пользователем.                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Исходные данные:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* 3/3*2+12/4/3-2                                                          *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
-            Console.WriteLine("***************************************************************************");
-            //Место решения задания
 
-            Console.WriteLine(ds.Calculate());
+            double x;
+            Console.WriteLine("Введите число x");
+            x = Convert.ToDouble(Console.ReadLine());
 
+            double y;
+            Console.WriteLine("Введите число y");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = Convert.ToDouble(ds.Calculate(x, y));
+
+            Console.WriteLine("Ответ = " + res);
             Console.ReadLine();
 
         }
